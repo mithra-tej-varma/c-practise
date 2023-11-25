5ee72f25e5335578ab8e2fcc35e332f434107e33
mithra-tej-varma <148879664+mithra-tej-varma@users.noreply.github.com>
2023-11-25 05:48:03 +0530

    Add files via upload
    first and second programs in c

---
+++ 01_practise.c
@@ -0,0 +1,8 @@
+//program to print hello world in c 
+/* Name: Mithra tej varma Nandimandalam
+    date: 24/11/23*/
+#include<stdio.h>
+int main(){
+  printf("Hello,world!");  
+  return 0;
+}
---
+++ 02_practise.c
@@ -0,0 +1,10 @@
+//To add to numbers in c 
+#include<stdio.h>
+int main(){
+    int a , b, c;
+    printf("Enter a and b value:");
+    scanf("%d%d",&a,&b);
+    c = a+b;
+    printf("%d + %d = %d",a,b,c);
+    return 0;
+}
